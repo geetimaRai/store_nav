3c4b248002d2809654cef10229618a627fe715ff
Geetima Rai <geetima.mnnit@gmail.com>
2013-09-14 00:41:49 -0700

    Commiting static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 StoreNav::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
   resources :departments_stores
 
   resources :department_stores
