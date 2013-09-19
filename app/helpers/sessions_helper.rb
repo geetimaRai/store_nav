cdfcef79eba6da82cedbf02237a748247a0a0a42
Geetima Rai <geetima.mnnit@gmail.com>
2013-09-18 22:52:09 -0700

    format

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,57 +5,57 @@ module SessionsHelper
 		self.current_user= user
 	end
 
-	def signed_in?
-!current_user.nil?
-end
+		def signed_in?
+		!current_user.nil?
+	end
 
 	def current_user?(user)
- user == current_user
-end
+	 	user == current_user
+	end
 
-def current_user=(user)
-@current_user = user
-end
+	def current_user=(user)
+		@current_user = user
+	end
 
-def current_user
+	def current_user
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
 
-@current_user ||= User.find_by_remember_token(cookies[:remember_token])
-end
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path, notice: "Please sign in."
+		end
+	end
 
-def signed_in_user
-unless signed_in?
-store_location
-redirect_to signin_path, notice: "Please sign in."
-end
-end
+	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_back_or user
+		else
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
 
-def create
-user = User.find_by_email(params[:session][:email])
-if user && user.authenticate(params[:session][:password])
-sign_in user
-redirect_back_or user
-else
-flash.now[:error] = 'Invalid email/password combination'
-render 'new'
-end
-end
-
-def sign_out
+	def sign_out
 		self.current_user = nil
 		cookies.delete(:remember_token)
 	end
 
-def destroy
-	sign_out
-	redirect_to root_path
-end
+	def destroy
+		sign_out
+		redirect_to root_path
+	end
 
-def redirect_back_or(default)
-redirect_to(session[:return_to] || default)
-session.delete(:return_to)
-end
-def store_location
-session[:return_to] = request.fullpath
-end
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+	
+	def store_location
+		session[:return_to] = request.fullpath
+	end
 
 end
